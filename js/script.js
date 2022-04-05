ea4024acc18c64ad4df1c32abe7446f0a9cc8f4b
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-04-05 09:43:20 -0400

    made it work
    ls
    
    '

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 document.querySelector('button').addEventListener('click', getFetch)
-document.querySelector('h2').innerText = localStorage.getItem('books');
+//document.querySelector('h2').innerText = localStorage.getItem('books');
 
 function getFetch(){
-  const isbn = document.querySelector('input').value
+  const book = document.querySelector('input').value
   
-  fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`)
+  fetch(`https://www.googleapis.com/books/v1/volumes?key=AIzaSyAfl0o4jdW9dsw1iVjT0vZmOPTaElBnP-E&q=intitle:${book}`)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        let titleInfo = data.items[0].volumeInfo;
+        console.log(titleInfo)
         //logs all book data
-        if(!localStorage.getItem('books')){
-          localStorage.setItem('books', data[`ISBN:${isbn}`].title);
-          //if local storage is falsy/null, put something in there. every other time the else will run
+        document.querySelector('h2').innerText = titleInfo.title
+        if(!titleInfo.averageRating){
+            document.querySelector('h3').innerText = ''
         } else {
-          let books = localStorage.getItem('books') + '\n' + data[`ISBN:${isbn}`].title;
-          localStorage.setItem('books', books);
-          //adds current title to books variable in local storage
+            document.querySelector('h3').innerText += `: ${titleInfo.averageRating}/5`
         }
-        
-        document.querySelector('h2').innerText = localStorage.getItem('books');
+        document.querySelector('h4').innerText += `:\n${titleInfo.description}`
+        //.accessInfo.volumeInfo.title
+        //document.querySelector('h2').innerText += ``;
         //puts book titles from local storage into the DOM
         //document.querySelector('img').src = data[`ISBN:${isbn}`].cover.medium;
         //puts image of book cover into the DOM
